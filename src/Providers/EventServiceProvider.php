7a43b442826b8f49d082be209d8c95c14ac02f8f
majidmohammadian <majeedmohammadian@gmail.com>
2024-10-15 17:48:08 +0330

    added event system provider

---
+++ src/Providers/EventServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace JobMetric\Vibe\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event to listener mappings for the application.
+     *
+     * @var array<class-string, array<int, class-string>>
+     */
+    protected $listen = [];
+}
