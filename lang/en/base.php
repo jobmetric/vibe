19c724df79825cd4818c9c6301739816693b64fd
majidmohammadian <majeedmohammadian@gmail.com>
2024-10-15 17:49:11 +0330

    added service provider and define panelio sections

---
+++ lang/en/base.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Vibe Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Vibe for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'description' => 'A dynamic CRM platform designed to enhance customer relationships, streamline interactions, and foster a positive engagement experience for businesses.',
+
+    'sections' => [
+        'board' => [
+            'name' => 'Boards',
+            'title' => 'Boards Management',
+        ],
+        'client' => [
+            'name' => 'Clients',
+            'title' => 'Clients Management',
+        ],
+        'team' => [
+            'name' => 'Teams',
+            'title' => 'Teams Management',
+        ],
+        'system' => [
+            'name' => 'System',
+            'title' => 'System Management',
+        ],
+        'report' => [
+            'name' => 'Reports',
+            'title' => 'Management Reports',
+        ],
+    ],
+
+];
---
+++ src/VibeServiceProvider.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace JobMetric\Vibe;
+
+use JobMetric\Vibe\Providers\EventServiceProvider;
+use JobMetric\PackageCore\Enums\RegisterClassTypeEnum;
+use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
+use JobMetric\Panelio\Facades\Panelio;
+
+class VibeServiceProvider extends PackageCoreServiceProvider
+{
+    /**
+     * @throws RegisterClassTypeNotFoundException
+     */
+    public function configuration(PackageCore $package): void
+    {
+        $package->name('vibe')
+            ->hasConfig()
+            ->hasTranslation()
+            ->hasRoute()
+            ->registerClass('event', EventServiceProvider::class, RegisterClassTypeEnum::REGISTER())
+            ->registerClass('Vibe', Vibe::class, RegisterClassTypeEnum::SINGLETON());
+    }
+
+    public function afterBootPackage(): void
+    {
+        Panelio::addPanel('vibe', [
+            'name' => config('vibe.panel_name'),
+            'namespace' => 'JobMetric\Vibe\Http\Controllers',
+            'args' => [
+                'description' => trans('vibe::base.description'),
+                'icon' => 'fas fa-user-shield',
+            ],
+            'permission' => 'vibe',
+            'position' => 0,
+        ]);
+
+        Panelio::addSection('vibe', 'board', [
+            'name' => trans('vibe::base.sections.board.name'),
+            'args' => [
+                'title' => trans('vibe::base.sections.board.title'),
+                'icon' => '<i class="ki-duotone ki-abstract-27 fs-2x"><span class="path1"></span><span class="path2"></span></i>',
+            ],
+            'permission' => 'vibe.board',
+            'position' => 0,
+        ]);
+
+        Panelio::addSection('vibe', 'client', [
+            'name' => trans('vibe::base.sections.client.name'),
+            'args' => [
+                'title' => trans('vibe::base.sections.client.title'),
+                'icon' => '<i class="ki-duotone ki-scroll fs-2x"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>',
+            ],
+            'permission' => 'vibe.client',
+            'position' => 0,
+        ]);
+
+        Panelio::addSection('vibe', 'team', [
+            'name' => trans('vibe::base.sections.team.name'),
+            'args' => [
+                'title' => trans('vibe::base.sections.team.title'),
+                'icon' => '<i class="ki-duotone ki-profile-user fs-2x"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i>',
+            ],
+            'permission' => 'vibe.team',
+            'position' => 0,
+        ]);
+
+        Panelio::addSection('vibe', 'system', [
+            'name' => trans('vibe::base.sections.system.name'),
+            'args' => [
+                'title' => trans('vibe::base.sections.system.title'),
+                'icon' => '<i class="ki-duotone ki-setting-3 fs-2x"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span></i>',
+            ],
+            'permission' => 'vibe.system',
+            'position' => 0,
+        ]);
+
+        Panelio::addSection('vibe', 'report', [
+            'name' => trans('vibe::base.sections.report.name'),
+            'args' => [
+                'title' => trans('vibe::base.sections.report.title'),
+                'icon' => '<i class="ki-duotone ki-chart-simple fs-2x"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i>',
+            ],
+            'permission' => 'vibe.report',
+            'position' => 0,
+        ]);
+    }
+}
